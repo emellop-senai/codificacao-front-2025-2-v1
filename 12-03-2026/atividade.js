84c9bb0aae3e37fdb164f6ad533c71b3cc608996
Professor <Professor@SN-385404.edu.fiesc.com.br>
2026-03-12 16:36:55 -0300

    correção atividade 1

--- 12-03-2026/atividade.js
+++ 12-03-2026/atividade.js
@@ -1,2 +1,36 @@
-let nome = 'Mariah'
-console.log(nome)
+// Exercício de soma
+
+// let num1 = 2
+// let num2 = 2
+
+// console.log("A soma é:", num1+num2);
+
+// Exercício de subtração
+// let num1 = 3
+// let num2 = 2
+
+// console.log("A subtração é:", num1 - num2);
+
+// Exercício de multiplicação
+// let num1 = 3
+// let num2 = 2
+
+// console.log("A multiplicação é:", num1 * num2);
+
+// Exercício de divisão 
+// let num1 = 4
+// let num2 = 2
+
+// console.log("A divisão é:", num1 / num2);
+
+// Exercício de modulo
+// let num1 = 5
+// let num2 = 2
+
+// console.log("O resto é:", num1 % num2);
+
+// Exercício de exponenciação
+let num1 = 5
+let num2 = 2
+
+console.log("O resto é:", num1 ** num2);
